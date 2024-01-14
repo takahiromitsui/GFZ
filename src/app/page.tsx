0e25b1b360e38350b355d937ef02f2a3df7dcd6f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-14 10:09:33 +0100

    feat: Create InteractiveMap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import dynamic from 'next/dynamic';
+
+const InteractiveMap = dynamic(() => import('@/components/InteractiveMap'), {
+	ssr: false,
+});
+
 export default function Home() {
-  return (
-    <main></main>
-  )
+	return (
+		<main>
+			<InteractiveMap />
+		</main>
+	);
 }
---
+++ src/components/InteractiveMap.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function InteractiveMap() {
+	return (
+		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+			/>
+		</MapContainer>
+	);
+}

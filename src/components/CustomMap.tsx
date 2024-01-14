99665f38cb0d94d4c0344222b8b1f8e81e7ca1f5
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-14 11:56:12 +0100

    feat: Change skin

--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -6,7 +6,7 @@ import MarkerClusterGroup from 'react-leaflet-cluster';
 import 'leaflet/dist/leaflet.css';
 
 export default function CustomMap() {
-	const position = [51.505, -0.09] as LatLngExpression;
+	const position = [0, 0] as LatLngExpression;
 	const customIcon = new Icon({
 		// iconUrl: require('../assets/marker-icon.png'),
 		iconUrl: 'https://cdn-icons-png.flaticon.com/512/149/149060.png',
@@ -42,7 +42,7 @@ export default function CustomMap() {
 		<MapContainer center={position} zoom={13}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
 			/>
 			<MarkerClusterGroup
 				chunkedLoading

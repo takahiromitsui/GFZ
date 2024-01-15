d1068de98af4094b78273e9ef3711da178bd1404
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-15 16:20:33 +0100

    feat: Move CustomMap and DataTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-'use client';
-import dynamic from 'next/dynamic';
-import { columns } from './columns';
-import { DataTable } from '@/components/data-table';
-
-import NetworkStationContextProvider, { NetworkStationContext } from '@/store/network-station-context';
+// import dynamic from 'next/dynamic';
 import StationControl from '@/components/station-control';
-import { useContext } from 'react';
+import NetworkStationContextProvider from '@/store/network-station-context';
 
-const CustomMap = dynamic(() => import('@/components/custom-map'), {
-	ssr: false,
-});
+// const CustomMap = dynamic(() => import('@/components/custom-map'), {
+// 	ssr: false,
+// });
 
-export default  function Home() {
-	const {stations} = useContext(NetworkStationContext);
+export default function Home() {
 	return (
 		<main className='flex mx-auto max-w-screen-lg'>
 			<NetworkStationContextProvider>
@@ -20,17 +14,15 @@ export default  function Home() {
 					{/* Station Control Component */}
 					<StationControl />
 				</div>
-				<div className='flex-1 flex flex-col ml-3'>
-					{/* CustomMap Component */}
-					<div className='mb-4'>
+				{/* <div className='flex-1 flex flex-col ml-3'>
+					<div className='mb-4 flex-grow'>
 						<CustomMap />
 					</div>
-					{/* DataTable Component */}
+
 					<section>
 						<h1 className='text-3xl font-bold mb-4'> All stations</h1>
-						<DataTable columns={columns} data = {stations}/>
 					</section>
-				</div>
+				</div> */}
 			</NetworkStationContextProvider>
 		</main>
 	);
--- src/store/network-station-context.tsx
+++ src/store/network-station-context.tsx
@@ -2,6 +2,9 @@
 import { useEffect, useState, createContext, ReactNode } from 'react';
 import { NetworkDataType, getNetworks } from '@/api/network';
 import { StationDataType, getStations } from '@/api/station';
+import { DataTable } from '@/components/data-table';
+import { columns } from '@/app/columns';
+import CustomMap from '@/components/custom-map';
 
 type NetworkStationContextType = {
 	networks: NetworkDataType[];
@@ -30,7 +33,9 @@ export default function NetworkStationContextProvider(props: {
 
 	useEffect(() => {
 		if (currentNetwork) {
-			getStations(currentNetwork).then(stations => setStations(stations));
+			getStations(currentNetwork).then(stations => {
+				setStations(stations);
+			});
 		}
 	}, [currentNetwork]);
 
@@ -39,6 +44,17 @@ export default function NetworkStationContextProvider(props: {
 			value={{ networks, setCurrentNetwork, stations }}
 		>
 			{props.children}
+			<div className='flex-1 flex flex-col ml-3'>
+				{/* CustomMap Component */}
+				<div className='mb-4 flex-grow'>
+					<CustomMap />
+				</div>
+				{/* DataTable Component */}
+				<section>
+					<h1 className='text-3xl font-bold mb-4'> All stations</h1>
+					<DataTable columns={columns} data={stations} />
+				</section>
+			</div>
 		</NetworkStationContext.Provider>
 	);
 }

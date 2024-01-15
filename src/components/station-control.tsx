4532e17f741a283dc8d58a3983c23735492bc3dd
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-15 15:02:56 +0100

    feat: Add search logic

--- src/components/station-control.tsx
+++ src/components/station-control.tsx
@@ -8,38 +8,76 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormLabel,
+	FormMessage,
+	FormItem,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
 import { NetworkStationContext } from '@/store/network-station-context';
 import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const FormSchema = z.object({
+	network: z.string(),
+});
 
 export default function StationControl() {
-	const { networks } = useContext(NetworkStationContext);
+	const { networks, setCurrentNetwork } = useContext(NetworkStationContext);
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+	});
+	const handleSubmit = (values: z.infer<typeof FormSchema>) => {
+		const network = networks.find(network => network.id === values.network);
+		if (network) {
+			setCurrentNetwork(network);
+		}
+	};
 	return (
-		<section>
-			<h1 className='text-3xl font-bold mb-4'> Station Information</h1>
-			<h1 className='text-3xl font-bold mb-4'> Network Code:</h1>
-			<Select>
-				<SelectTrigger className='w-[300px]'>
-					<SelectValue placeholder='All Networks' />
-				</SelectTrigger>
-				<SelectContent>
-					<SelectGroup>
-						<SelectLabel>Networks</SelectLabel>
-						{networks.map(item => {
-							const startYear = new Date(item.startTime).getFullYear();
-							const formattedString = `${item.network} (${startYear}) - ${item.description}`;
-							return (
-								<SelectItem
-									key={item.network}
-									value={item.network}
-									// className='w-[300px] overflow-ellipsis whitespace-nowrap'
-								>
-									{formattedString}
-								</SelectItem>
-							);
-						})}
-					</SelectGroup>
-				</SelectContent>
-			</Select>
-		</section>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-8'>
+				<FormField
+					control={form.control}
+					name='network'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Network Code:</FormLabel>
+							<Select onValueChange={field.onChange}>
+								<FormControl>
+									<SelectTrigger className='w-[300px]'>
+										<SelectValue placeholder='All Networks' />
+									</SelectTrigger>
+								</FormControl>
+								{/* Content */}
+								<SelectContent>
+									<SelectGroup>
+										<SelectLabel>Networks</SelectLabel>
+										{networks.map((item, index) => {
+											const { id, network, startTime, description } = item;
+											const key = id;
+											const startYear = new Date(startTime).getFullYear();
+											const formattedString = `${network} (${startYear}) - ${description}`;
+											return (
+												<SelectItem key={key} value={id}>
+													{formattedString}
+												</SelectItem>
+											);
+										})}
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+							{/* message */}
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit'>Search</Button>
+			</form>
+		</Form>
 	);
 }

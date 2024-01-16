ab5f79b84109bf19a3860603b3a41bb5bacc3c54
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-16 20:29:44 +0100

    feat: Changer header width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import NetworkStationContextProvider from '@/store/network-station-context';
 export default function Home() {
 	return (
 		<>
-			<header className='w-full bg-blue-600 text-white p-10 font-bold'>
+			<header className='w-screen bg-blue-600 text-white p-10 font-bold'>
 				<h1 className='text-2xl'>Interactive Map</h1>
 			</header>
 			<main className='flex mt-10 ml-10'>

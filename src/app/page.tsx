e373a1506454dee71096f8c94d8455564da09974
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-01-16 21:00:34 +0100

    feat: Remove margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 			<header className='w-screen bg-blue-600 text-white p-10 font-bold'>
 				<h1 className='text-2xl'>Interactive Map</h1>
 			</header>
-			<main className='flex mt-10 justify-between mx-20'>
+			<main className='flex mt-10'>
 				<NetworkStationContextProvider>
 					<StationControl />
 				</NetworkStationContextProvider>
